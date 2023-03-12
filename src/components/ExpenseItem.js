1738819fb3be5be3506a66fad68c38fab3e1c5af
iammeliaskhan <eliaskhan353@gmail.com>
2023-03-12 17:08:46 -0400

    Mar 12 v3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseDate = new Date(2023, 3, 12);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 454;
+
   return (
     <div className="expense-item">
-      <div>March 12th 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$454</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
