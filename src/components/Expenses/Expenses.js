c4e3f1d14352cfd91c9d9c1a1a47a8f8669147f1
iammeliaskhan <eliaskhan353@gmail.com>
2023-03-18 13:51:01 -0400

    outputting cond. content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,6 +15,21 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent =
+      filteredExpenses.length > 0 &&
+      filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        ></ExpenseItem>
+      ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -23,14 +38,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         ></ExpensesFilter>
 
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          ></ExpenseItem>
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
